10b62c5e70bc75f037d36e4d4ec482178aeced32
samanthadevlin <64792728+samanthadevlin@users.noreply.github.com>
2020-09-19 10:43:59 -0400

    createchart

--- StarterCode/assets/js/app.js
+++ StarterCode/assets/js/app.js
@@ -1,5 +1,24 @@
 // @TODO: YOUR CODE HERE!
 
+// Chart Params
+var svgWidth = 960;
+var svgHeight = 500;
+
+var margin = { top: 20, right: 40, bottom: 60, left: 50 };
+
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+// Create an SVG wrapper, append an SVG group that will hold our chart, and shift the latter by left and top margins.
+var svg = d3
+  .select("body")
+  .append("svg")
+  .attr("width", svgWidth)
+  .attr("height", svgHeight);
+
+var chartGroup = svg.append("g")
+.attr("transform", `translate(${margin.left}, ${margin.top})`);
+
 function updateData(sel, data) {
     sel.data(data);
 
@@ -20,6 +39,7 @@ function updateData(sel, data) {
     sel.exit().remove()
 }
 
+// Import data from an external CSV file
 d3.csv("assets/data/data.csv").then((data) => {
     console.table(data.slice(0,3))
     let smolData = data.slice(0,3);
